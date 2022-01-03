e7f3b0245fc76acce947bbd7d91d72c0e91c0e78
uratamanabu <mabu_ip@msn.com>
2022-01-03 20:42:32 +0900

    #37

--- micro-api/src/main/java/mabubu0203/com/github/cafe/api/controller/location/helper/response/LocationResponseMapper.java
+++ micro-api/src/main/java/mabubu0203/com/github/cafe/api/controller/location/helper/response/LocationResponseMapper.java
@@ -1,6 +1,9 @@
 package mabubu0203.com.github.cafe.api.controller.location.helper.response;
 
 import com.netflix.dgs.codegen.types.Location;
+import com.netflix.dgs.codegen.types.LocationAddress;
+import com.netflix.dgs.codegen.types.LocationContact;
+import com.netflix.dgs.codegen.types.LocationHours;
 import mabubu0203.com.github.cafe.api.service.location.model.output.LocationServiceOutput;
 import mabubu0203.com.github.cafe.common.controller.helper.response.ResponseMapper;
 
@@ -11,8 +14,49 @@ public class LocationResponseMapper
   public Location apply(LocationServiceOutput output) {
     return new Location.Builder()
         .code(output.locationCode())
+        .name(output.name())
+        .contact(this.contact(output.contact()))
+        .address(this.address(output.address()))
+        .openDate(output.openDate())
+        .closeDate(output.closeDate())
+        .hours(this.hours(output.hours()))
         .memo(output.memo())
         .version(output.version())
+        .build();
+  }
+
+  private LocationContact contact(LocationServiceOutput.ContactObject object) {
+    return new LocationContact.Builder()
+        .phoneNumber(object.phoneNumber())
+        .mailAddress(object.mailAddress())
+        .build();
+  }
+
+  private LocationAddress address(LocationServiceOutput.AddressObject object) {
+    return new LocationAddress.Builder()
+        .postalCode(object.postalCode())
+        .prefectureCode(object.prefectureCode())
+        .prefectureLabel(object.prefectureLabel())
+        .address1(object.address1())
+        .address2(object.address2())
+        .address3(object.address3())
+        .streetAddress(object.streetAddress())
+        .buildingName(object.buildingName())
+        .supplement(object.supplement())
+        .build();
+  }
+
+  private LocationHours hours(LocationServiceOutput.HoursObject object) {
+
+//    Optional.ofNullable(object.getOpeningTime())
+//        .map(lt -> lt.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)))
+//        .ifPresent(hours::setOpeningTime);
+//    Optional.ofNullable(object.getClosingTime())
+//        .map(lt -> lt.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)))
+//        .ifPresent(hours::setClosingTime);
+
+    return new LocationHours.Builder()
+        .supplement(object.supplement())
         .build();
   }
 
--- micro-api/src/main/java/mabubu0203/com/github/cafe/api/service/location/impl/converter/output/LocationServiceOutputConverter.java
+++ micro-api/src/main/java/mabubu0203/com/github/cafe/api/service/location/impl/converter/output/LocationServiceOutputConverter.java
@@ -10,6 +10,36 @@ public class LocationServiceOutputConverter
   @Override
   public LocationServiceOutput apply(LocationEntity locationEntity) {
     return LocationServiceOutput.builder()
+        .locationCode(locationEntity.getLocationCodeValue())
+        .name(locationEntity.name())
+        .contact(
+            LocationServiceOutput.ContactObject.builder()
+                .phoneNumber(locationEntity.getPhoneNumberValue())
+                .mailAddress(locationEntity.getMailAddressValue())
+                .build()
+        )
+        .address(
+            LocationServiceOutput.AddressObject.builder()
+                .postalCode(locationEntity.getPostalCodeValue())
+                .prefectureCode(locationEntity.getPrefectureCode())
+                .prefectureLabel(locationEntity.getPrefectureLabel())
+                .address1(locationEntity.address1())
+                .address2(locationEntity.address2())
+                .address3(locationEntity.address3())
+                .streetAddress(locationEntity.streetAddress())
+                .buildingName(locationEntity.buildingName())
+                .supplement(locationEntity.getAddressSupplementValue())
+                .build())
+        .openDate(locationEntity.openDate())
+        .closeDate(locationEntity.closeDate())
+        .hours(
+            LocationServiceOutput.HoursObject.builder()
+                .openingTime(locationEntity.openingTime())
+                .closingTime(locationEntity.closingTime())
+                .supplement(locationEntity.getHoursSupplementValue())
+                .build())
+        .memo(locationEntity.getMemoValue())
+        .version(locationEntity.getVersionValue())
         .build();
   }
 
