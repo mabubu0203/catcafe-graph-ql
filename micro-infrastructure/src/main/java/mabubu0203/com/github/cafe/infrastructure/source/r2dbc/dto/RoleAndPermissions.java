d67819242253b0c5d890f1db46b89a7baee5ec41
uratamanabu <mabu_ip@msn.com>
2022-04-27 09:31:55 +0900

    #93

--- micro-infrastructure/src/main/java/mabubu0203/com/github/cafe/infrastructure/source/r2dbc/dto/RoleAndPermissions.java
+++ micro-infrastructure/src/main/java/mabubu0203/com/github/cafe/infrastructure/source/r2dbc/dto/RoleAndPermissions.java
@@ -1,10 +1,14 @@
 package mabubu0203.com.github.cafe.infrastructure.source.r2dbc.dto;
 
 import lombok.Value;
+import org.springframework.data.relational.core.mapping.Column;
 
 @Value
 public class RoleAndPermissions {
 
+  @Column(value = "role_key")
   String roleKey;
+
+  @Column(value = "permission_keys")
   String permissionKeys;
 }
